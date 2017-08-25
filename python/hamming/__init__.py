3a645a3efa533e1a6824fcdda0f2c4ce6e182488
James Black <jamesblack975@gmail.com>
2017-08-24 21:36:53 -0500

    Added a version that uses loops

--- python/hamming/__init__.py
+++ python/hamming/__init__.py
@@ -7,9 +7,8 @@
 def compute(s1, s2):
     if len(s1) != len(s2):
         raise ValueError("Undefined for sequences of unequal length")
-#    n = 0 
-#    for i in range(0, len(s1)): 
-#       if s1[i] != s2[i]:
-#          n += 1
-#    return n
-    return distance.levenshtein(s1, s2, normalized=True)
+    n = 0 
+    for i in range(0, len(s1)): 
+       if s1[i] != s2[i]:
+          n += 1
+    return n
