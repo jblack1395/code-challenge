0c6cb25d727a6112c28b86542ea89d4ddfb27baa
James Black <jamesblack975@gmail.com>
2017-08-24 22:01:19 -0500

    Initial code, using for loop.

--- javascript/hamming/index.js
+++ javascript/hamming/index.js
@@ -3,5 +3,19 @@
 // your code meets all of the conditions.
 // You may use ES6 or ES5 to solve.
 
+function Hamming() { }
+
+Hamming.prototype.compute = function(s1, s2) {
+    if(s1.length != s2.length) throw 'DNA strands must be of equal length.';
+    var num = 0;
+    for (x = 0; x < s1.length; x++) {
+      if (s1[x] != s2[x]) {
+        num++;
+      }
+    }
+    return num;
+  };
 
 module.exports = Hamming;
+
+
